fb2bfbee9744fb40da1f632cf33e0b44efeff64e
Roman Myachin <roma.mellon@mail.ru>
2025-09-14 15:50:14 +0500

    add code

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 from pathlib import Path
 import os
+
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -58,8 +61,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
 }
 
---
+++ library/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from library.apps import LibraryConfig
+
+app_name = LibraryConfig.name
+
+urlpatterns = [
+    path('',),
+]
